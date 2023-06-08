c0d46119eb700fae30cf85d1bfa19fbedf1bcf23
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2023-06-08 08:24:40 +0300

    Initial commit

---
+++ src/test/java/com/vburcovschi/spring/springboot/spring_datarest/SpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vburcovschi.spring.springboot.spring_datarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
